846ae01f640ab0249d97184f42ca3bbb0ae0eeec
Alexey Parkhomov <115148133+AlexGekko88@users.noreply.github.com>
2024-04-20 11:19:30 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
 
-        MyTelegramBot myTelegramBot = new MyTelegramBot("NASA_24_bot","7013766453:AAELqnaqolmSDMhSk_inlNi1MFkWnh4-hhE");
+        MyTelegramBot myTelegramBot = new MyTelegramBot("NASA_24_bot",");
 
 
     }
